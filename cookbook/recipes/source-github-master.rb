0b41f755b34553bb8610939e31f66036a87f1292
Alan Sebastian <alan_sebastian@rapid7.com>
2020-10-22 14:44:30 -0400

    Add source ref attribute

--- cookbook/recipes/source-github-master.rb
+++ cookbook/recipes/source-github-master.rb
@@ -25,7 +25,7 @@ package 'git'
 
 git 'guardian-source' do
   repository Guardian::Helpers.github_uri(node['guardian']['repo'])
-  revision 'master'
+  revision ['guardian']['source']['ref']
   destination node['guardian']['path']
   depth 1
 
